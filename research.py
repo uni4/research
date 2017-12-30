c44de305a99efb2a8d1810db7a4e53ada6f1ac34
uni4 <mut.anony@gmail.com>
2017-12-30 15:20:37 +0900

    クロージングとオープニングを追加

--- research.py
+++ research.py
@@ -6,28 +6,30 @@ import numpy as np
 import sys
 
 def mask(param):
-	# 画像を取得
-	img = cv2.imread("%s"%param[1])
+    # 画像を取得
+    img = cv2.imread("%s"%param[1])
 
-	# フレームをHSVに変換
-	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # フレームをHSVに変換
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	# 取得する色の範囲を指定する
-	lower = np.array([0, 30, 160])
-	upper = np.array([30, 150, 255])
+    # 取得する色の範囲を指定する
+    lower = np.array([0, 30, 160])
+    upper = np.array([30, 150, 255])
 
-	# 指定した色に基づいたマスク画像の生成
-	img_mask = cv2.inRange(hsv, lower, upper)
+    # 指定した色に基づいたマスク画像の生成
+    img_mask = cv2.inRange(hsv, lower, upper)
 
-	#フレーム画像とマスク画像の共通の領域を抽出する。
-	img_color = cv2.bitwise_and(img, img, mask=img_mask)
-	#img_color = cv2.cvtColor(img_color, cv2.COLOR_HSV2BGR)
+    #フレーム画像とマスク画像の共通の領域を抽出する。
+    img_color = cv2.bitwise_and(img, img, mask=img_mask)
+    #img_color = cv2.cvtColor(img_color, cv2.COLOR_HSV2BGR)
 
-	return img_color
+    cv2.imwrite("color.jpg", img_color)
+
+    return img_color
 
 def labelling(im):
     height, width = im.shape[:2]
-    copy_im = im.copy()
+    kernel = np.ones((5,5),np.uint8)
 
     # グレースケール変換
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -40,7 +42,6 @@ def labelling(im):
 
     data = np.delete(label[2], 0, 0)
     max_index = np.argsort(data[:,4])[::-1][0] + 1
-
     dst = label[1].reshape((-1))
 
     for index in range(len(dst)):
@@ -62,6 +63,10 @@ def labelling(im):
     im_con = cv2.drawContours(im_re, contours, 0, (255,255,255), -1,)
 
     cv2.imwrite("kekka.jpg",im_con)
+    opening = cv2.morphologyEx(im_con, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(im_con, cv2.MORPH_CLOSE, kernel)
+    cv2.imwrite("opening.jpg", opening)
+    cv2.imwrite("closing", closing)
 
 if __name__ == '__main__':
     param = sys.argv
