f3a0843add547c4f7d5608b7971ce96c1b0aef64
uni4 <mut.anony@gmail.com>
2018-01-12 19:06:56 +0900

    複数の写真を同時に処理できるようにした

--- research.py
+++ research.py
@@ -7,10 +7,26 @@ from scipy.spatial import distance
 import time
 import print as pt
 
+#ベクトルの正規化
+def normalize(v, axis=-1, order=2):
+	l2 = np.linalg.norm(v, ord = order, axis=axis, keepdims=True)
+	l2[l2==0] = 1
+	return v/l2
+
+#最大値と最小値を使った正規化
+def min_max(x, axis=None):
+    mi = min(x)
+    ma = max(x)
+    result = (x-mi)/(ma-mi)
+    return result
+
+
 #肌色領域のみを抽出
-def mask(param):
+def mask(param,p_number):
+	print("肌色領域抽出開始")
+	print(p_number)
 	# 画像を取得
-	img = cv2.imread("%s"%param[1])
+	img = cv2.imread("%s"%param[p_number])
 
 	# フレームをHSVに変換
 	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -27,12 +43,13 @@ def mask(param):
 	#img_color = cv2.cvtColor(img_color, cv2.COLOR_HSV2BGR)
 
 	cv2.imwrite("color.jpg", img_color)
-	print("肌色領域抽出")
+	print("肌色領域抽出終了")
 
 	return img_color
 
 #最大面積を抽出することでノイズを除去
-def labelling(im):
+def labelling(im,p_number):
+	print("最大面積領域の抽出開始")
 	height, width = im.shape[:2]
 	kernel = np.ones((10,10),np.uint8)
 
@@ -45,15 +62,16 @@ def labelling(im):
 	max_index = np.argsort(data[:,4])[::-1][0] + 1
 	dst = label[1].reshape((-1))
 
-	print("最大面積領域の抽出開始")
-		
-	dst = np.array([255 if dst[index] == max_index else 0 for index in range(len(dst))])
+	#最大面積のラベル番号の部分は255にして、それ以外を0にする
+	dst[dst==max_index] = 255
+	dst[dst!=255] = 0
 
 	dst = dst.reshape((height, width))
 	cv2.imwrite("kekka_totyu.jpg",dst)
-	print("最大面積領域を抽出")
+	print("最大面積領域を抽出終了")
 	im_re = cv2.imread("kekka_totyu.jpg")
 
+	print("クロージング処理開始")
 	# グレースケール変換
 	gray2 = cv2.cvtColor(im_re, cv2.COLOR_BGR2GRAY)
 	gray2 = cv2.threshold(gray2, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
@@ -64,13 +82,13 @@ def labelling(im):
 
 	cv2.imwrite("kekka.jpg",im_con)
 	closing = cv2.morphologyEx(im_con, cv2.MORPH_CLOSE, kernel)
-	cv2.imwrite("closing10.jpg", closing)
+	cv2.imwrite("closing10_" + str(p_number) + ".jpg", closing)
 	print("クロージング処理終了")
 
 	return closing
 
 #指先を抽出する処理
-def yubisaki(im):
+def yubisaki(im,p_number):
 
 	height,width = im.shape[:2]
 	gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -90,19 +108,40 @@ def yubisaki(im):
 	dst = label[1]
 	list = []
 	list2 = []
+	list_hen = []
 	a = np.array([y_center,x_center])
-	print("1a")
-	
+	print("重心と各画素の距離を算出開始")
+	tt1 = time.time()
+
 	for y in range(height):
 		for x in range(width):
 			if dst[y][x] == max_index:
 				b = np.array([y,x])
 				list.append(distance.euclidean(a,b))
 
+	print("重心と各画素の距離を算出終了")
 	#ave = np.mean(list)
+	print(type(list))
+	list = min_max(list)
+	print("list",list)
 	ave = np.average(list)
-	print("1b")
+	print("ave",ave)
+	print("指先の抽出開始")
 
+	i = 0
+	for y in range(height):
+		for x in range(width):
+			if dst[y][x] == max_index:
+				deviation = list[i] - ave
+				i += 1
+				list2.append(deviation)
+				#print(deviation)
+				if (deviation) < ave - 0.08:
+					im[y][x] = (0,0,0)
+
+
+
+	"""
 	for y in range(height):
 		for x in range(width):
 			if dst[y][x] == max_index:
@@ -111,21 +150,25 @@ def yubisaki(im):
 				list2.append(dis - ave)
 				if (dis - ave) < 800:
 					im[y][x] = (0,0,0)
+	"""
 
-	print("1c")
+	tt2 = time.time()
+	print("2重ループ処理の時間",tt2- tt1)
+	print("指先の抽出終了")
 	ave2 = np.average(list2)
 	su = np.sum(list2)
-	cv2.imwrite("saki.jpg", im)
+	cv2.imwrite("saki" + str(p_number) + ".jpg", im)
 
 	return im
 
 def main():
-	print(param)
 	t1 = time.time() 
 
-	gazou = labelling(mask(param))
-	gazou2 = yubisaki(gazou)
-	pt.finger_edit(param, gazou2)
+	p_number = 1
+	for p_number in range(1,len(param)):
+		gazou = labelling(mask(param,p_number),p_number)
+		gazou2 = yubisaki(gazou,p_number)
+		pt.finger_edit(param, gazou2,p_number)
 
 	t2 = time.time()
 	elapsed_time = t2-t1
