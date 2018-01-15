cfd2d81adae087d893516b977344e16e4282d0ba
uni4 <mut.anony@gmail.com>
2018-01-15 12:44:09 +0900

    閾値の変更

--- research.py
+++ research.py
@@ -65,6 +65,7 @@ def labelling(im,p_number):
 	#最大面積のラベル番号の部分は255にして、それ以外を0にする
 	dst[dst==max_index] = 255
 	dst[dst!=255] = 0
+	#dst = np.where(dst==1, 255, 0)
 
 	dst = dst.reshape((height, width))
 	cv2.imwrite("kekka_totyu.jpg",dst)
@@ -113,17 +114,25 @@ def yubisaki(im,p_number):
 	print("重心と各画素の距離を算出開始")
 	tt1 = time.time()
 
+	
 	for y in range(height):
 		for x in range(width):
 			if dst[y][x] == max_index:
 				b = np.array([y,x])
 				list.append(distance.euclidean(a,b))
 
+	"""
+	y = range(0,height)
+	x = range(0,width)
+	#list = np.array([(y,x) for y in y for x in x distance.euclidean(a,[y,x]) if dst[y][x] == max_index])
+	list = np.array([(y,x) for y in y for x in x if dst[y][x] == max_index])
+	list = distance.euclidean(a,list)
+	"""
+
 	print("重心と各画素の距離を算出終了")
 	#ave = np.mean(list)
-	print(type(list))
 	list = min_max(list)
-	print("list",list)
+	#print("list",list)
 	ave = np.average(list)
 	print("ave",ave)
 	print("指先の抽出開始")
@@ -136,7 +145,7 @@ def yubisaki(im,p_number):
 				i += 1
 				list2.append(deviation)
 				#print(deviation)
-				if (deviation) < ave - 0.08:
+				if (deviation) < ave - 0.1:
 					im[y][x] = (0,0,0)
 
 
