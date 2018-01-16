c90f9bda9e14ae7b09840c46c23c96f1aca0e61d
uni4 <mut.anony@gmail.com>
2018-01-16 21:41:20 +0900

    カスケード分類器に基づき識別するプログラム

---
+++ recognition.py
@@ -0,0 +1,27 @@
+#https://note.nkmk.me/python-opencv-face-detection-haar-cascade/
+import cv2
+import numpy as np
+import sys
+
+def main():
+	hand_cascade_path = '/Users/dennomaaya/Desktop/py/cascade_directory/cascade/test_data/cascade.xml'
+
+	hand_cascade = cv2.CascadeClassifier(hand_cascade_path)
+
+
+	img = cv2.imread("%s"%param[1])
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	hands = hand_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+	for (x, y, w, h) in hands:
+	    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 5)
+	    roi_gray = gray[y: y + h, x: x + w]
+	    roi_color = img[y: y + h, x: x + w]
+
+	cv2.imwrite('hand_detect.jpg', img)
+
+
+if __name__ == '__main__':
+	param = sys.argv
+	main()
